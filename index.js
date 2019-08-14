171a0140e0d52f7d41ea2b839ae4e12f3a5ec259
frueda14 <fabian.rueda14@gmail.com>
2019-08-13 19:39:39 -0500

    cambios realizados durante la sesion

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+// los modulos locales importados al proyecto, pueden ser usados aqui cowsay por ejemplo
+// para los modulos externos que esten dentro de node_modules podemos hacer referencia
+// a ellos simplemente llamandolos por su nombre.
+
+// con esta declaracion ya podemos utilizar el modulo cowsay, Ojo se importan nombres sin rutas
+const cowsay = require("cowsay");
+
+
+console.log(cowsay.say({
+	text : "Hello I'm cowsay",
+	e : "@-@",
+	T : "U "
+}));

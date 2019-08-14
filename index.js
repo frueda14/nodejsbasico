3635151544bfb6b2a7a06597ec9e58c372a8c802
frueda14 <fabian.rueda14@gmail.com>
2019-08-14 07:29:42 -0500

    Avances en la case de promesas en node

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call.js");
+
+
+//console.log(call.sync("Fabian"));
+// arrow function ()=>
+/*
+call.withCallback("Fabian R", ()=> {
+    call.sync
+});
+*/
+
+//call.withCallback("Fabian Rueda", call.sync);
+// Pending Succesfull o Rejected --> Estos son los 3 estados que manejan las promesas
+//console.log(call.withPromise("Frq"));
+
+// el .then es utilizado cuando se llaman promesas, en ese bloque se obtiene la respuesta
+// y se hace algo con dicha respuesta, en esta caso imprimir el nombre.
+call.withPromise("Frq")
+    .then
+    (
+        name=>
+        {
+            console.log(name);
+        }
+    );
--- src/call.js
+++ src/call.js
@@ -1,12 +1,13 @@
 module.exports = {
     sync: function(nombre) {
-        return "Hola ${nombre}! Bienvenido a Node.js";
+        return `Hola ${nombre}! Bienvenido a Node.js`;
     },
     withCallback: function(nombre, cb) {
         setTimeout(()=>{
-            cb(nombre)
+            console.log(cb(nombre))
         }, 5000);
     },
+    // Promise, resolve y reject son parametros establecidos para el uso de Promesas
     withPromise: function(nombre) {
         return new Promise((resolve, reject)=>{
             setTimeout(()=>{

6e83c52a1af52038e833ee3f35dd9e142e025311
frueda14 <fabian.rueda14@gmail.com>
2019-08-15 11:41:40 -0500

    Avances usando readline

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const readline = require("readline");
+// se crea una interfaz de entrada y salida
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+const q = [
+    "Cual es tu primer nombre ? ",
+    "Cual es tu primer apellido ? ",
+    "Cual es tu edad ? "
+];
+
+const AskQuestion = (rl, question) => 
+{
+    return new Promise((resolve, rej) => 
+    {
+        rl.question(question, answer => 
+        {
+            resolve(answer);
+        })
+    });
+};
+
+const Ask = function(questions)
+{
+    return new Promise(async resolve => 
+    {
+        let results = [];
+        for (let i = 0; i < questions.length; i++)
+        {
+            // importante el await al momento de llamar AskQuestion await indica
+            // que el programa debe esperar hasta que termine su ejecucion
+            // si lo quitamos el programa ejecuta y sigue con las demas lineas
+            // de codigo
+            const result = await AskQuestion(rl, questions[i]);
+            results.push(result);
+        }
+        rl.close();
+        resolve(results);
+    });
+};
+
+// "resultado" usado en el then, es el resultado obtenido despues de la ejecucion
+Ask(q)
+    .then(resultado => 
+    {
+        console.log(`Hola ${resultado[0]} ${resultado[1]} tu edad es: ${resultado[2]}`);
+    });

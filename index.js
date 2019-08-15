cdae68420ffbbd0f3188c31c48ea73437b011252
frueda14 <fabian.rueda14@gmail.com>
2019-08-15 07:48:46 -0500

    Avances realizados en clase

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-/**
- * 
- * *************
- * Comience aqui
- * *************
- * 
- */
+const call = require("./src/call.js");
+
+
+//call.greet("Fabian Rueda");
+
+/* esto se puede escribir de otra forma utilizando async y await
+call.withPromise("Fabian", "Rueda")
+    .then(n=>console.log(n)); */
+
+async function callWithPromise()
+{
+    // await indica que espere hasta que la promesa se ejecute 
+    const fullName = await call.withPromise("Fabian", "Rueda");
+    console.log(fullName);
+}
+
+callWithPromise();
